0a1af4dbdfee4885115a24e609e114496735308f
hilatsivion <81438607+hilatsivion@users.noreply.github.com>
2023-05-30 10:53:30 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+// crate a route
+app.get("/", (req, res) => {
+  res.send("hello from the server");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
